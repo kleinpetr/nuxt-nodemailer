c3b60c12c4a38f27e3566a958f0d1c88b5e261c0
Klein Petr <petr@kleinpetr.com>
2024-05-29 18:05:27 +0200

    fix: useRuntimeConfig import

--- src/runtime/server/useNodeMailer.ts
+++ src/runtime/server/useNodeMailer.ts
@@ -1,8 +1,8 @@
 import { createTransport } from 'nodemailer'
 import type SMTPTransport from 'nodemailer/lib/smtp-transport'
+import { useRuntimeConfig } from '#imports'
 
 export const useNodeMailer = () => {
-  // @ts-ignore can't see auto-imports in that context
   const { nodemailer } = useRuntimeConfig()
   const transport = createTransport(nodemailer as SMTPTransport.Options)
 
