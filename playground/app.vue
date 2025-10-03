6f3a09230e5a228b130a46f90f2251ee9b3d60b5
Ashrakk <davidevertigocuni@gmail.com>
2025-10-03 09:01:32 +0200

    Version 1.1.3 - chores and enhancements

--- playground/app.vue
+++ playground/app.vue
@@ -5,4 +5,15 @@
 </template>
 
 <script setup>
+
+onMounted(async () => {
+  try {
+    const data = await $fetch('/api/send-mail');
+    console.log('Email sent!')
+    console.log(data)
+  } catch (error) {
+    console.error('Failed to send email:', error)
+  }
+})
+
 </script>
--- playground/server/api/test.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(() => {
-  console.log(useRuntimeConfig().nodemailer)
-  const { sendMail } = useNodeMailer()
-  return sendMail({ subject: 'Nuxt + nodemailer', text: 'Hello from nuxt-nodemailer!', to: 'petr@kleinpetr.com' })
-})
